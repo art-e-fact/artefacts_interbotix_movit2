18a5b3ec2e264200c12f696c730f62365bb3e6f0
decarabas <codrea.eric@gmail.com>
2023-09-19 17:29:49 -0500

    Changed to cartesian path planner for demo

--- src/artefacts_demo_control/artefacts_demo_control/interbotix_moveit_control_node.py
+++ src/artefacts_demo_control/artefacts_demo_control/interbotix_moveit_control_node.py
@@ -1,9 +1,9 @@
 """
-Example of moving to a pose goal.
-`ros2 run pymoveit2 ex_pose_goal.py --ros-args -p position:="[0.25, 0.0, 1.0]" -p quat_xyzw:="[0.0, 0.0, 0.0, 1.0]" -p cartesian:=False`
+Example of pick and place node
 """
 
 from threading import Thread
+import time
 
 import rclpy
 from rclpy.callback_groups import ReentrantCallbackGroup
@@ -13,22 +13,15 @@ from pymoveit2 import MoveIt2
 from pymoveit2 import MoveIt2Gripper
 from pymoveit2.robots import interbotix as panda
 
-import time
-
 def main():
     rclpy.init()
 
     # Create node for this example
     node = Node("artefacts_control")
 
-    # node.declare_parameter("position", [0.4, 0.0, 0.3])
-    node.declare_parameter("position", [0.416925, 0.0, 0.02])
-    node.declare_parameter("quat_xyzw", [0.0, 0.0, 0.0, 1.0])
-    node.declare_parameter("cartesian", False)
-
     callback_group = ReentrantCallbackGroup()
 
-    # Create MoveIt 2 interface
+    # Create MoveIt 2 Arm Interface
     moveit2 = MoveIt2(
         node=node,
         joint_names=panda.joint_names(),
@@ -46,7 +39,6 @@ def main():
         closed_gripper_joint_positions=panda.CLOSED_GRIPPER_JOINT_POSITIONS,
         gripper_group_name=panda.MOVE_GROUP_GRIPPER,
         callback_group=callback_group,
-        # skip_planning = True,
         follow_joint_trajectory_action_name = "/wx200/gripper_controller/follow_joint_trajectory"
     )
 
@@ -56,15 +48,9 @@ def main():
     executor_thread = Thread(target=executor.spin, daemon=True, args=())
     executor_thread.start()
 
-    # Get parameters
-    # position = node.get_parameter("position").get_parameter_value().double_array_value
-    quat_xyzw = node.get_parameter("quat_xyzw").get_parameter_value().double_array_value
-    cartesian = node.get_parameter("cartesian").get_parameter_value().bool_value
+    node.declare_parameter("quat_xyzw", [0.0, 0.0, 0.0, 1.0])
+    node.declare_parameter("cartesian", True)
 
-    # Move to pose
-    # node.get_logger().info(
-    #     f"Moving to {{position: {list(position)}, quat_xyzw: {list(quat_xyzw)}}}"
-    # )
 
     position_init = [0.4, 0.0, 0.3]
     position_travel = [0.3, 0.1, 0.15]
@@ -84,6 +70,8 @@ def main():
                             -1.02974,
                             0.0,
                         ]
+    quat_xyzw = node.get_parameter("quat_xyzw").get_parameter_value().double_array_value
+    cartesian = node.get_parameter("cartesian").get_parameter_value().bool_value
 
     # Init
     moveit2.move_to_pose(position=position_init, quat_xyzw=quat_xyzw, cartesian=cartesian, target_link = "wx200/ee_gripper_link")
@@ -133,6 +121,16 @@ def main():
     rclpy.shutdown()
     exit(0)
 
-
+# def move_arm_to_pose(self):
+#     moveit2.move_to_pose(position=position_init, quat_xyzw=quat_xyzw, cartesian=cartesian, target_link = "wx200/ee_gripper_link")
+#     moveit2.wait_until_executed()
+# def move_arm_joints(self):
+#     pass
+# def place(self):
+#     pass
+# def transfer(self):
+#     pass
+# def arm_init(self):
+#     pass
 if __name__ == "__main__":
     main()
